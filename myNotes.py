e2a8b93715ef5a6858d9f7cbc0eea50ab39467b1
Lakshmi Jinkala <lakshmi.sai.shyma@icloud.com>
2025-10-25 23:27:10 -0500

    Update myNotes.py

--- myNotes.py
+++ myNotes.py
@@ -1,3 +1,3 @@
 # github works similar to git
 # it is also used for the hiring procress to see a students' project 
-# its also a great place
+# its also a great place to share documents related to a project with a group all in one place
